e719636406fa4e6a079b205d169b1205357a15f8
Guilherme de Barros da Rocha <guilhermebarrosr@hotmail.com>
2017-06-13 23:34:41 -0300

    Inicio

--- RelatóriosDKSOFT/Form1.cs
+++ RelatóriosDKSOFT/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace RelatóriosDKSOFT
 {
@@ -15,6 +16,50 @@ namespace RelatóriosDKSOFT
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGerarRelatorio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string select = gerarSelect();
+
+                FileStream file = new FileStream("C:\\Users\\guilh\\Desktop\\select.txt", FileMode.OpenOrCreate);
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    writer.Write(select);
+                }
+                               
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show(err.Message);
+            }
+          
+            
+        }
+        private string gerarSelect()
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.Append("SELECT ALUNOS.NOME");
+            foreach (Control item in gpbxCampos.Controls)
+            {
+                if (item.GetType() == typeof(ckbxCampo))
+                {
+                    if (((ckbxCampo)item).Checked)
+                    {
+                        str.Append(((ckbxCampo)item).Campo);
+                    }
+
+                }
+
+            }
+            str.Append(@" FROM ALUNOS LEFT JOIN ALUNOS_CURSOS ON ALUNOS.ID_ALUNO = ALUNOS_CURSOS.ID_ALUNO LEFT JOIN TURMAS ON TURMAS.ID_TURMA = ALUNOS_CURSOS.ID_TURMA LEFT JOIN CIDADES ON ALUNOS.ID_CIDADE = CIDADES.ID_CIDADE ");
+            str.Append(@" WHERE ALUNOS.TIPO = 'AL';");
+
+            return str.ToString();
         }
     }
 }
---
+++ RelatóriosDKSOFT/ckbxCampo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelatóriosDKSOFT
+{
+    public class ckbxCampo : CheckBox
+    {
+        public string Campo { get; set; }
+
+    }
+}
